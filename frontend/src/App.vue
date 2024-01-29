6a4d40004a95fc3482fff92ae25db7ebb229850e
Pyae Sone Paing <www.pyaesonekm2003@gmail.com>
2024-01-29 16:02:05 +0630

    frontend setup code

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,5 +1,23 @@
 <template>
-  <h1 class="text-3xl font-bold underline">
-    Hello World
-  </h1>
+      <h1 class="text-3xl font-bold underline">
+      Hello World
+    </h1>
 </template>
+<script>
+import HelloWorld from "./components/HelloWorld.vue"
+export default {
+  data() {
+    return {
+      fronteEnd: null
+    }
+  },
+  components:{
+    HelloWorld
+  }
+}
+</script>
+<style>
+  h1 {
+    color: red;
+  }
+</style>
